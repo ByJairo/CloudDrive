8c81b6f596ac746b64a7b76330c87c0568b34043
ByJairo <103023573+ByJairo@users.noreply.github.com>
2022-05-12 14:10:20 +0800

    NO

--- src/components/SortToggler.vue
+++ src/components/SortToggler.vue
@@ -1,8 +1,20 @@
 <script setup>
-$END$
+import {reactive} from 'vue'
+
+const toggle = () => {
+  sort.order = sort.order === "asc" ? "desc" : "asc"
+}
+const sort = reactive({
+  column: "name",
+  order: "asc"
+})
 </script>
 <template>
-  <div>SortToggler</div>
+  <button @click="toggle" class="rounded-button">
+
+    <icon-arrow-up v-if="sort.order==='asc'"/>
+    <icon-arrow-down v-else/>
+  </button>
 </template>
 
 <style scoped>
